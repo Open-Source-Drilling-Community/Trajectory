8cd0547b717a9d7c179a0f5922cb9afe48007f3c
GillesNorce <gipe@norceresearch.no>
2026-02-21 17:44:54 +0100

    Improving plot lifecycle (Scatter3DPlot)

--- ModelTest/Tests.cs
+++ ModelTest/Tests.cs
@@ -12,44 +12,6 @@ namespace NORCE.Drilling.Trajectory.ModelTest
         {
         }
 
-        [Test]
-        public void Test_Calculus()
-        {
-            Guid guid = Guid.NewGuid();
-            MetaInfo metaInfo = new() { ID = guid };
-            DateTimeOffset creationDate = DateTimeOffset.UtcNow;
-
-            Guid guid2 = Guid.NewGuid();
-            MetaInfo metaInfo2 = new() { ID = guid2 };
-            DateTimeOffset creationDate2 = DateTimeOffset.UtcNow;
-            ScalarDrillingProperty derivedData1Param = new() { DiracDistributionValue = new DiracDistribution() { Value = 2.0 } };
-            Model.DerivedData1 derivedData1 = new() { DerivedData1Param = derivedData1Param };
-            SurveyPoint surveyPoint = new()
-            {
-                MetaInfo = metaInfo2,
-                Name = "My test SurveyPoint name",
-                Description = "My test SurveyPoint for POST",
-                CreationDate = creationDate,
-                LastModificationDate = creationDate2,
-                SurveyPointParam = new ScalarDrillingProperty() { DiracDistributionValue = new DiracDistribution() { Value = 1.0 } },
-                DerivedData1 = derivedData1,
-                Type = SurveyPointType.DerivedData1
-            };
-            Model.Trajectory trajectory = new()
-            {
-                MetaInfo = metaInfo,
-                Name = "My test Trajectory",
-                Description = "My test Trajectory",
-                CreationDate = creationDate,
-                LastModificationDate = creationDate,
-                SurveyPointList = [surveyPoint],
-            };
-
-            Assert.That(trajectory.OutputParam, Is.Null);
-            trajectory.Calculate();
-            Assert.That(trajectory.OutputParam, Is.EqualTo(3));
-        }
-
         [OneTimeTearDown]
         public void OneTimeTearDown()
         {
--- WebApp/Shared/DataUtils.cs
+++ WebApp/Shared/DataUtils.cs
@@ -1,4 +1,6 @@
-﻿public static class DataUtils
+﻿using NORCE.Drilling.Trajectory.ModelShared;
+
+public static class DataUtils
 {
     // default values
     public static string FLOATING_COLOUR = "rgba(70, 50, 240, 0.86)";
@@ -19,5 +21,116 @@
         UnitAndReferenceParameters.UnitSystemName = (string)val;
     }
 
-   
+    public static string[] COLORSCALE = ["black", "blue", "grey", "red", "orange", "green", "yellow", "pink", "brown", "purple"];
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param nameList="">name of each curve in the list to plot</param>
+    /// <param modeFlagList="">modeFlag of each curve in the list to plot (1 = lines; 2 = markers)</param>
+    /// <param colorList="">color of each curve in the list of curves to plot</param>
+    /// <param northValuesList="">North values for the list of curves to plot</param>
+    /// <param eastValuesList="">East values for the list of curves to plot</param>
+    /// <param TVDValuesList="">TVD values for the list of curves to plot</param>
+    /// <param trajectoryList=""></param>
+    public static void UpdatePlots(
+        List<string> nameList,
+        List<int> modeFlagList,
+        List<string> colorList,
+        List<List<object>> northValuesList,
+        List<List<object>> eastValuesList,
+        List<List<object>> TVDValuesList,
+        List<Trajectory> trajectoryList
+        )
+    {
+        if (trajectoryList is { })
+        {
+            //clear data from scatter plot
+            nameList.Clear();
+            modeFlagList.Clear();
+            colorList.Clear();
+            northValuesList.Clear();
+            eastValuesList.Clear();
+            TVDValuesList.Clear();
+            // vSectValuesList.Clear();
+
+            //generate only one curve for the current trajectory
+            for (int k = 0; k < trajectoryList.Count; ++k)
+            {
+                if (trajectoryList[k].SurveyStationList is { Count: > 2 } ssList &&
+                    trajectoryList[k].InterpolatedTrajectory is { Count: > 2 } traj)
+                {
+                    //////////////////////////////////////
+                    /// Interpolated trajectory (lines) //
+                    //////////////////////////////////////
+                    //Retrieve and compute data points for interpolated trajectory (plotted as lines)
+                    List<object> northValues = [];
+                    List<object> eastValues = [];
+                    List<object> tvdValues = [];
+                    List<object> vSectValues = [];
+
+                    SurveyPoint? prevPoint = traj[0];
+                    //double vSect = 0.0;
+                    for (int i = 0; i < traj.Count; ++i)
+                    {
+                        SurveyPoint? point = traj[i];
+                        if (point is { } &&
+                            point.X is { } x &&
+                            point.Y is { } y &&
+                            point.Z is { } z)
+                        {
+                            northValues.Add(x);
+                            eastValues.Add(y);
+                            tvdValues.Add(z);
+                            //vSect += System.Math.Sqrt(System.Math.Pow((double)(point.X - prevPoint.X), 2.0) + System.Math.Pow((double)(point.Y - prevPoint.Y), 2.0));
+                            //vSectValues.Add(vSect);
+                            prevPoint = point;
+                        }
+                    }
+                    northValuesList.Add(northValues);
+                    eastValuesList.Add(eastValues);
+                    TVDValuesList.Add(tvdValues);
+                    // vSectValuesList.Add(vSectValues);
+                    nameList.Add(string.IsNullOrEmpty(trajectoryList[k].Name) ? "traj (interp)" : trajectoryList[k].Name + "(interp)");
+                    modeFlagList.Add(1); // 1=lines, 2=markers, 3=lines+markers
+                    colorList.Add(k < COLORSCALE.Length ? COLORSCALE[k] : "black");
+
+                    /////////////////////////////////////
+                    /// SurveyStation points (markers) //
+                    /////////////////////////////////////
+                    //Retrieve and compute data points for SurveyStation (plotted as markers)
+                    List<object> northValues2 = [];
+                    List<object> eastValues2 = [];
+                    List<object> tvdValues2 = [];
+                    //List<object> vSectValues2 = [];
+
+                    SurveyStation prevStation = ssList[0];
+                    // vSect = 0.0;
+                    for (int i = 0; i < ssList.Count; ++i)
+                    {
+                        SurveyStation? station = ssList[i];
+                        if (station is { } &&
+                            station.X is { } x &&
+                            station.Y is { } y &&
+                            station.Z is { } z)
+                        {
+                            northValues2.Add(x);
+                            eastValues2.Add(y);
+                            tvdValues2.Add(z);
+                            // vSect += System.Math.Sqrt(System.Math.Pow((double)(point.X - prevPoint.X), 2.0) + System.Math.Pow((double)(point.Y - prevPoint.Y), 2.0));
+                            // vSectValues2.Add(vSect);
+                            prevStation = station;
+                        }
+                    }
+                    northValuesList.Add(northValues2);
+                    eastValuesList.Add(eastValues2);
+                    TVDValuesList.Add(tvdValues2);
+                    // vSectValuesList.Add(vSectValues2);
+                    nameList.Add(string.IsNullOrEmpty(trajectoryList[k].Name) ? "traj (survey stations)" : trajectoryList[k].Name + "(survey stations)");
+                    modeFlagList.Add(2); // 1=lines, 2=markers, 3=lines+markers
+                    colorList.Add(k < COLORSCALE.Length ? COLORSCALE[k] : "black");
+                }
+            }
+        }
+    }
 }
